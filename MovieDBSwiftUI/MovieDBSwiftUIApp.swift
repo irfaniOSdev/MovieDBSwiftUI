58fc26494212944a215da294509c8d15119803a2
Muhammad Irfan <M.mohammed@aiondigital.com>
2024-04-04 00:50:33 +0500

    Initial Commit

---
+++ MovieDBSwiftUI/MovieDBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBSwiftUIApp.swift
+//  MovieDBSwiftUI
+//
+//  Created by Muhammad Irfan on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
